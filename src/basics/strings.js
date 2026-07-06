4791964ebc9ced413d2caab62c46e897c3669e20
gonsanto <goncalosantofilipe8@gmail.com>
2026-07-06 14:42:39 +0200

    feat: add function find and replace preserving case

--- src/basics/strings.js
+++ src/basics/strings.js
@@ -7,5 +7,16 @@
  * @return {string} the resulting string, with all needle words transformed to newWord
  */
 export function findAndReplacePreservingCase(needle, haystack, newWord) {
-  // Write your code here
+  if (typeof newWord !== "string" || typeof needle !== "string") {
+    throw Error;
+  }
+
+  const matchCasing = (oldWord, newWord) => {
+    return newWord.substring(0, oldWord.length).split("").map((char, index) => {
+      const oldChar = oldWord[index]; return oldChar === oldChar.toUpperCase() ? char.toUpperCase() : char.toLowerCase();
+    }).join("");
+  };
+
+  const regex = RegExp(needle, "gi");
+  return haystack.split(" ").map(needle => regex.test(needle) ? matchCasing(needle, newWord) : needle).join(" ");
 }
